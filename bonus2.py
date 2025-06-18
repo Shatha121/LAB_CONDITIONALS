5d1c5a9f1a622852ec74f5f1dc481ca213a5ff17
shatha <shatha25200@gmail.com>
2025-06-18 13:21:49 +0300

    I fixed Answer.py and bonus2.py

--- bonus2.py
+++ bonus2.py
@@ -5,7 +5,8 @@ Email = input("Enter your Email: ")
 
 if len(username) <= 2:
     print("the name length must be more than 2 characters, please provide a valid name.")
-elif Email.find("@gmail") == -1:
+elif not Email.endswith("@gmail.com") or Email.find("@") == 0 or Email.count("@") != 1:
     print("the email is not valid , please provide a valid email .")
+
 else:
     print(f"welcome {username} , you registered with the email {Email} !")
